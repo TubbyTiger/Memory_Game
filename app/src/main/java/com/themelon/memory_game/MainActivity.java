ca35d1468b928c496714fbfb01f87592171f7624
TubbyTiger <krntime@gmail.com>
2016-01-04 14:08:33 -0600

    Image Button clicked ==>> sound playback

--- app/src/main/java/com/themelon/memory_game/MainActivity.java
+++ app/src/main/java/com/themelon/memory_game/MainActivity.java
@@ -1,8 +1,11 @@
 package com.themelon.memory_game;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer redBeep = MediaPlayer.create(this, R.raw.beep1);
+        ImageButton RedButton = (ImageButton)this.findViewById(R.id.RedButton);
+        RedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) {
+                redBeep.start();
+            }
+            });
     }
 
 }
